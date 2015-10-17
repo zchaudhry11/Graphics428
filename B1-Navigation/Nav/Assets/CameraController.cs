4e11776985c7cbc68367122ce0cbd41588c98ce0
Rush Sangs <rush.sang@rutgers.edu>
2015-10-17 14:22:26 -0400

    added camera controls and movable cube

---
+++ B1-Navigation/Nav/Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement;
+		
+		movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		transform.position += movement;
+	}
+}
---
+++ B1-Navigation/Nav/Assets/ObstacleMoveTo.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleMoveTo : MonoBehaviour {
+	private NavMeshAgent navAgent;
+	//private Animator anim;
+	private bool select;
+	private bool set = false;
+	private Vector3 MoveD;
+	// Use this for initialization
+	void Start () {
+		navAgent = GetComponent<NavMeshAgent>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (select && set)
+		{
+			navAgent.destination = MoveD;
+			//anim.SetTrigger("Move");
+		}
+		if (navAgent.pathStatus == NavMeshPathStatus.PathComplete)
+		{
+			set = false;
+		}
+	}
+	
+	void Select()
+	{
+		Debug.Log("Cube Logged");
+		select = true;
+	}
+	
+	void Deselect(int x)
+	{
+		Debug.Log("Cube Delogged");
+		select = false;
+	}
+
+	void Destination (Vector3 dest)
+	{
+		Debug.Log("Destination Set");
+		MoveD = dest;
+		set = true;
+	}
+}
