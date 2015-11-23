76282adb33f7b72b55f5a7381e0f97a1a8d60f6c
Crazydriver8 <krazydriver8@gmail.com>
2015-11-23 18:20:51 -0500

    3 wandering idiots

--- KADAPT-master/Assets/MyBehaviorTree.cs
+++ KADAPT-master/Assets/MyBehaviorTree.cs
@@ -7,7 +7,10 @@ public class MyBehaviorTree : MonoBehaviour
 	public Transform wander1;
 	public Transform wander2;
 	public Transform wander3;
-	public GameObject participant;
+    public Transform wander4;
+    public Transform wander5;
+
+    public GameObject participant;
 
 	private BehaviorAgent behaviorAgent;
 	// Use this for initialization
@@ -27,16 +30,28 @@ public class MyBehaviorTree : MonoBehaviour
 	protected Node ST_ApproachAndWait(Transform target)
 	{
 		Val<Vector3> position = Val.V (() => target.position);
-		return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+        return new Sequence(participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
 	}
 
 	protected Node BuildTreeRoot()
 	{
-		return
-			new DecoratorLoop(
-				new SequenceShuffle(
-					this.ST_ApproachAndWait(this.wander1),
-					this.ST_ApproachAndWait(this.wander2),
-					this.ST_ApproachAndWait(this.wander3)));
+		
+        if (participant == null)
+        {
+
+            return new DecoratorLoop(this.ST_ApproachAndWait(this.wander1));
+        }
+        else
+        {
+            return new DecoratorLoop(
+                new SequenceShuffle(
+                    this.ST_ApproachAndWait(this.wander1),
+                    this.ST_ApproachAndWait(this.wander2),
+                    this.ST_ApproachAndWait(this.wander3),
+                    this.ST_ApproachAndWait(this.wander4),
+                    this.ST_ApproachAndWait(this.wander5)
+                 )
+            );
+        }
 	}
 }
